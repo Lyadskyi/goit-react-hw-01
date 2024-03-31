7cd8fd30cd8d0a1270081a9a5c278c1ce1d42fdf
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-03-31 23:50:38 +0300

    Add social network user in the task-1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+// import './App.css';
+
+import Profile from '../Profile/Profile';
+import userData from '../../userData.json';
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+}
