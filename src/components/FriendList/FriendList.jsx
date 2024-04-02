07916c61481766233b8892ba85662ced3bc9c29d
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-04-03 00:49:38 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,18 @@
 import FriendListItem from '../FriendListItem/FriendListItem';
-import css from '../FriendList/FriendList.module.css'
+import css from '../FriendList/FriendList.module.css';
 
-const FriendList = ({ friends }) => (
-  <ul className={css.list}>
-    {friends.map(el => (
-      <li key={el.id}>
-        <FriendListItem
-          avatar={el.avatar}
-          name={el.name}
-          isOnline={el.isOnline}
-        />
-      </li>
-    ))}
-  </ul>
-);
-
-export default FriendList;
+export default function FriendList({ friends }) {
+  return (
+    <ul className={css.list}>
+      {friends.map(el => (
+        <li key={el.id}>
+          <FriendListItem
+            avatar={el.avatar}
+            name={el.name}
+            isOnline={el.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,18 @@
 import css from '../FriendListItem/FriendListItem.module.css';
 
-const FriendListItem = ({ avatar, name, isOnline }) => (
-  <div className={css.listItem}>
-    <img
-      className={css.avatar}
-      src={avatar}
-      alt={`Avatar of ${name}`}
-      width="48"
-    />
-    <p className={css.name}>{name}</p>
-    <span className={isOnline ? css.online : css.offline}>
-      {isOnline ? 'Online' : 'Offline'}
-    </span>
-  </div>
-);
-
-export default FriendListItem;
+export default function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <div className={css.listItem}>
+      <img
+        className={css.avatar}
+        src={avatar}
+        alt={`Avatar of ${name}`}
+        width="48"
+      />
+      <p className={css.name}>{name}</p>
+      <span className={isOnline ? css.online : css.offline}>
+        {isOnline ? 'Online' : 'Offline'}
+      </span>
+    </div>
+  );
+}
